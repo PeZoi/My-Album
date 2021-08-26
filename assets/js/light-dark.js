774a708b9bf525f33f3b73d0bc198a62ca000289
PeZoi <pezoiks1@gmail.com>
2021-08-26 23:37:40 +0700

    Day37

---
+++ assets/js/light-dark.js
@@ -0,0 +1,84 @@
+var checkbox_toggle = document.getElementById('light-dark')
+
+//Background Body
+checkbox_toggle.addEventListener('change', function () {
+  document.body.classList.toggle('dark')
+})
+
+//Background Header
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelector('.header').classList.toggle('dark')
+})
+
+//Color Text
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.header-menu-link')[0].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.header-menu-link')[1].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.header-menu-link')[2].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.header-menu-link')[3].classList.toggle('dark')
+})
+
+//Background Sub Menu
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelector('.sub-menu').classList.toggle('dark')
+})
+
+//Color Text
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.sub-menu-link')[0].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.sub-menu-link')[1].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.sub-menu-link')[2].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.sub-menu-link')[3].classList.toggle('dark')
+})
+
+//Shadow
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelector('.intro-img-item').classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelector('.intro-info').classList.toggle('dark')
+})
+
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[0].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[1].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[2].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[3].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[4].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[5].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[6].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[7].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[8].classList.toggle('dark')
+})
+checkbox_toggle.addEventListener('change', function () {
+  document.querySelectorAll('.member-img')[9].classList.toggle('dark')
+})
+
